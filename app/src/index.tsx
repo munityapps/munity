7ff98bac78fe708a4ce6c47e5be87d5592133a97
cyril <cyril@munityapps.com>
2021-12-09 11:47:42 +0100

    fix: add service worker, add authentication api package

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -20,6 +20,8 @@ import OvermindNavbarRight from './overmind/components/NavbarComponents/NavbarRi
 
 // Configuration
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 // Style
 import './styles.scss';
@@ -65,6 +67,11 @@ ReactDOM.render(
     </ReduxProvider>,
     document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
